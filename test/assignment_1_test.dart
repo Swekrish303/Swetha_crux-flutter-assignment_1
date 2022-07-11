73f49bbc87fcb031cf75ecbf1f12dee7a9fe898b
Akshat Oke <76596238+Akshat-Oke@users.noreply.github.com>
2022-07-11 09:06:34 +0530

    Update assignment_1_test.dart

--- test/assignment_1_test.dart
+++ test/assignment_1_test.dart
@@ -1,5 +1,5 @@
-// import 'package:assignment_1/assignment_1.dart';
-import 'package:assignment_1/sol.dart';
+ import 'package:assignment_1/assignment_1.dart';
+//import 'package:assignment_1/sol.dart';
 import 'package:test/test.dart';
 
 void main() {
