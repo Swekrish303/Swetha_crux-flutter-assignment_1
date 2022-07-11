969b7fd788166f67427a390055beb55d5e46be25
Swekrish303 <kswetha036@gmail.com>
2022-07-12 05:03:26 +0530

    Modified code (solved)

--- lib/assignment_1.dart
+++ lib/assignment_1.dart
@@ -11,26 +11,35 @@ class Stack<T> implements StackInterface<T> {
   /// You can only use this list for implementing the methods
   final _plates = <T>[];
   @override
-  bool get isEmpty => throw UnimplementedError();
+  bool get isEmpty => _plates.isEmpty;
 
   @override
-  int get length => throw UnimplementedError();
+  int get length => _plates.length;
 
   @override
   T pop() {
-    // TODO: implement pop
-    throw UnimplementedError();
+    if (_plates.isEmpty) {
+      throw Exception("Stack Empty");
+    } else {
+      return _plates.removeLast();
+    }
+    //throw UnimplementedError();
   }
 
   @override
   void push(T) {
-    // TODO: implement push
+    _plates.add(T);
+    //throw UnimplementedError();
   }
 
   @override
   T top() {
-    // TODO: implement top
-    throw UnimplementedError();
+    if (_plates.isEmpty) {
+      throw Exception("Stack Empty");
+    } else {
+      return _plates.last;
+    }
+    //throw UnimplementedError();
   }
 }
 
@@ -48,22 +57,34 @@ class Pancake {
   int get hashCode => serialNumber;
 }
 
-// TODO: implement PancakeHandler class which extends Stack
 class PancakeHandler extends Stack<Pancake> {
-  // TODO: Override `pop` method
   @override
   Pancake pop([int n = 1]) {
-    return _plates.first;
+    if (_plates.isNotEmpty) {
+      _plates.length = _plates.length - (n - 1);
+      return _plates.removeLast();
+    } else {
+      throw Exception("Stack Empty");
+    }
+    //return _plates.first;
   }
 
   Pancake remove(int from, int to) {
+    if (_plates[to].hasTopping || _plates[from - 1].hasTopping) {
+      throw Exception("Has Syrup");
+    } else {
+      _plates.removeRange(from + 1, to + 1);
+    }
     return _plates.removeAt(from);
   }
 }
 
 extension ChefsPancakeHandler on PancakeHandler {
-  int inspect(bool Function(Pancake) predicate) {
-    // TODO: implement inspect
-    throw UnimplementedError();
+  int inspect(bool Function(Pancake) check) {
+    int n1 = _plates.length;
+    _plates.removeWhere((Pancake p) => check(p) == false);
+    return (n1 - _plates.length);
+
+    //throw UnimplementedError();
   }
 }
